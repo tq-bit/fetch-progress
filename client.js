7de85bcbe1c3312266226788c1c6f5f2f14c973d
Tobias Quante <t.quante@outlook.com>
2021-08-16 17:37:34 +0200

    ref: adjust files as per blog article

--- client.js
+++ client.js
@@ -7,7 +7,7 @@ export default function http(rootUrl) {
 
   let controller = null;
 
-  const json = async (path, options, ...args) => {
+  const json = async (path, options,) => {
     _resetLocals();
     let signal = controller.signal;
     loading = true
@@ -16,8 +16,8 @@ export default function http(rootUrl) {
       const response = await fetch(rootUrl + path, { signal, ...options });
 
       if (response.status >= 200 && response.status < 300) {
-        results = await _readBody(response);
-        return JSON.parse(results);
+        results = await _readBody(response)
+        return JSON.parse(results)
       } else {
         throw new Error(response.statusText)
       }
@@ -30,10 +30,43 @@ export default function http(rootUrl) {
     }
   }
 
-  const cancel = () => {
-    _resetLocals();
-    controller.abort();
-  };
+  const _readBody = async (response) => {
+    const reader = response.body.getReader();
+    const length = +response.headers.get('content-length');
+    let received = 0;
+
+    // Loop through the response stream and extract data chunks
+    while (loading) {
+      const { done, value } = await reader.read();
+      const payload = { detail: { received, length, loading } }
+      const onProgress = new CustomEvent('fetch-progress', payload);
+      const onFinished = new CustomEvent('fetch-finished', payload)
+
+      if (done) {
+        // Finish loading
+        loading = false;
+        window.dispatchEvent(onFinished)
+      } else {
+        // Push values to the chunk array
+        chunks.push(value);
+        received += value.length;
+        window.dispatchEvent(onProgress)
+      }
+    }
+
+    // Concat the chinks into a single array
+    let body = new Uint8Array(received);
+    let position = 0;
+
+    // Order the chunks by their respective position
+    for (let chunk of chunks) {
+      body.set(chunk, position);
+      position += chunk.length;
+    }
+
+    // Decode the response and return it
+    return new TextDecoder('utf-8').decode(body);
+  }
 
   const _resetLocals = () => {
     loading = false;
@@ -45,42 +78,10 @@ export default function http(rootUrl) {
     controller = new AbortController();
   }
 
-  const _readBody = async (response) => {
-    const reader = response.body.getReader();
-    const length = +response.headers.get('content-length');
-    let received = 0;
-    while (loading) {
-      const { done, value } = await reader.read();
-      const payload = { detail: { received, length, loading } }
-      const onProgress = new CustomEvent('fetch-progress', payload);
-      const onFinished = new CustomEvent('fetch-finished', payload)
-
-      if (done) {
-        // Finish loading and dispatch finish event
-        loading = false;
-        window.dispatchEvent(onFinished)
-      } else {
-        // Push values to the chunk array
-        chunks.push(value);
-        received += value.length;
-
-        // dispatch progress event
-        window.dispatchEvent(onProgress)
-      }
-    }
-
-    // Concat the chinks into a single array
-    let body = new Uint8Array(received);
-    let position = 0;
-
-    for (let chunk of chunks) {
-      body.set(chunk, position);
-      position += chunk.length;
-    }
-
-    // dispatch finished eventx
-    return new TextDecoder('utf-8').decode(body);
-  }
+  const cancel = () => {
+    _resetLocals();
+    controller.abort();
+  };
 
   return { json, cancel }
 }
--- main.js
+++ main.js
@@ -1,35 +1,35 @@
+// Import the fetch client and initalize it
 import http from './client.js';
+
+// Grab the DOM elements
 const progressbar = document.getElementById('progress-bar');
 const progressbutton = document.getElementById('fetch-button');
 const abortbutton = document.getElementById('abort-button');
 const progresslabel = document.getElementById('progress-label');
+const { json, cancel } = http('http://universities.hipolabs.com/');
 
-const client = http('http://universities.hipolabs.com/');
-
-window.addEventListener('fetch-progress', (e) => {
-  const { received, length, loading } = e.detail;
+const setProgressbarValue = (payload) => {
+  const { received, length, loading } = payload;
   const value = ((received / length) * 100).toFixed(2);
   progresslabel.textContent = `Download progress: ${value}%`;
   progressbar.value = value;
-});
+};
 
-window.addEventListener('fetch-finished', (e) => {
-  const { received, length, loading } = e.detail;
-  const value = ((received / length) * 100).toFixed(2);
-  progresslabel.textContent = `Download progress: ${value}%`;
-  progressbar.value = value;
-});
-
+// Bind the fetch function to the button's click event
 progressbutton.addEventListener('click', async () => {
-  try {
-    const test = await client.json('search?country=United+States');
-    console.log(test);
-  } catch (error) {
-    console.log(error);
-  }
+  const universities = await json('search?country=United+States');
+  console.log(universities);
 });
 
 abortbutton.addEventListener('click', () => {
-  client.cancel()
+  cancel()
   alert('Request has been cancelled')
 })
+
+window.addEventListener('fetch-progress', (e) => {
+  setProgressbarValue(e.detail);
+});
+
+window.addEventListener('fetch-finished', (e) => {
+  setProgressbarValue(e.detail);
+});
